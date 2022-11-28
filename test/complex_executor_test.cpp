527a6cbe158fa2c0b752bdbd1f8d5610a8b73b72
Cesartwothousands <cesarchen616@outlook.com>
2022-11-27 23:55:00 -0500

    11272354

--- test/complex_executor_test.cpp
+++ test/complex_executor_test.cpp
@@ -23,50 +23,133 @@ using std::endl;
 using std::vector;
 
 int main() {
-  Table t1;
-  for (int i = 0; i < 15; i++) {
-    t1.insert(i, i, std::to_string(i));
-  }
 
-  Table t2;
-  for (int i = 5; i < 20; i++) {
-    t2.insert(i, i, std::to_string(i));
-  }
+    Table t1;
 
-  Table t3;
-  for (int i = 0; i < 4; i++) {
-    t3.insert(i, 3, std::to_string(9));
-  }
+    for (int i = 5; i < 25; i++) {
 
-  Table t4;
-  for (int i = 4; i < 10; i++) {
-    for (int j = 0; j < 2; j++) {
-      t4.insert(i * 2 + j, i, std::to_string(i));
+        t1.insert(i, i, std::to_string(i));
+
     }
-  }
 
-  FilterPredicate pred1(15, PredicateType::LESS);
-  FilterPredicate pred2(5, PredicateType::GREATER);
+    Table t2;
 
-  SeqScanExecutor scan1(&t1);
-  FilterSeqScanExecutor scan2(&t2, &pred1);
-  SeqScanExecutor scan3(&t3);
-  FilterSeqScanExecutor scan4(&t4, &pred2);
+    for (int i = 0; i < 15; i++) {
 
-  SimpleHashFunction hash_fn1("val1");
-  SimpleHashFunction hash_fn2("val2");
+        t2.insert(i, i, std::to_string(i));
 
-  HashJoinExecutor hash1(&scan1, &scan2, &hash_fn1);
-  HashJoinExecutor hash2(&scan3, &scan4, &hash_fn2);
+    }
 
-  NestedLoopJoinExecutor executor(&hash1, &hash2, "val1");
+    Table t3;
 
-  executor.Init();
-  Tuple tuple;
-  while (executor.Next(&tuple)) {
-    cout << "Successfully get tuple! id: " << tuple.id
-         << " val1: " << tuple.val1 << " val2: " << tuple.val2 << endl;
-  }
+    for (int i = 4; i < 10; i++) {
 
-  return 0;
+        for (int j = 0; j < 2; j++) {
+
+            t3.insert(i * 2 + j, i, std::to_string(i));
+
+        }
+
+    }
+
+    Table t4;
+
+    for (int i = 0; i < 2; i++) {
+
+        t4.insert(i, 10, std::to_string(9));
+
+    }
+
+    for (int i = 2; i < 5; i++) {
+
+        t4.insert(i, 11, std::to_string(3));
+
+    }
+
+    FilterPredicate pred1(15, PredicateType::LESS);
+
+    FilterPredicate pred2(3, PredicateType::GREATER);
+
+    FilterSeqScanExecutor scan1(&t1, &pred1);
+
+    FilterSeqScanExecutor scan2(&t2, &pred2);
+
+    SeqScanExecutor scan3(&t3);
+
+    SeqScanExecutor scan4(&t4);
+
+    SimpleHashFunction hash_fn1("val1");
+
+    SimpleHashFunction hash_fn2("val2");
+
+    HashJoinExecutor hash1(&scan1, &scan2, &hash_fn1);
+
+    HashJoinExecutor hash2(&scan3, &scan4, &hash_fn2);
+
+    NestedLoopJoinExecutor executor(&hash1, &hash2, "val1");
+
+    Tuple tuple;
+
+    scan1.Init();
+    cout << "Scan Function 1:\n";
+    while (scan1.Next(&tuple)) {
+        cout << "Successfully get tuple! id: " << tuple.id << " val1: " << tuple.val1 << " val2: " << tuple.val2 << endl;
+    }
+    cout << "\n";
+
+    scan2.Init();
+    cout << "Scan Function 2:\n";
+    while (scan2.Next(&tuple)) {
+        cout << "Successfully get tuple! id: " << tuple.id << " val1: " << tuple.val1 << " val2: " << tuple.val2 << endl;
+    }
+    cout << "\n";
+
+    scan3.Init();
+    cout << "Scan Function 3:\n";
+    while (scan3.Next(&tuple)) {
+        cout << "Successfully get tuple! id: " << tuple.id << " val1: " << tuple.val1 << " val2: " << tuple.val2 << endl;
+    }
+    cout << "\n";
+
+    scan4.Init();
+    cout << "Scan Function 4:\n";
+    while (scan4.Next(&tuple)) {
+        cout << "Successfully get tuple! id: " << tuple.id << " val1: " << tuple.val1 << " val2: " << tuple.val2 << endl;
+    }
+    cout << "\n";
+
+    hash1.Init();
+    cout << "Hash Function 1:\n";
+    while (hash1.Next(&tuple)) {
+        cout << "Successfully get tuple! id: " << tuple.id << " val1: " << tuple.val1 << " val2: " << tuple.val2 << endl;
+    }
+    cout << "\n";
+
+    hash2.Init();
+    cout << "Hash Function 2:\n";
+    while (hash2.Next(&tuple)) {
+        cout << "Successfully get tuple! id: " << tuple.id << " val1: " << tuple.val1 << " val2: " << tuple.val2 << endl;
+    }
+    cout << "\n";
+
+    executor.Init();
+    cout << "Output:\n";
+    while (executor.Next(&tuple)) {
+        cout << "Successfully get tuple! id: " << tuple.id << " val1: " << tuple.val1 << " val2: " << tuple.val2 << endl;
+    }
+
+    /*executor.Init();
+
+    Tuple tuple;
+
+    while (executor.Next(&tuple)) {
+
+        cout << "Successfully get tuple! id: " << tuple.id
+
+             << " val1: " << tuple.val1 << " val2: " << tuple.val2 << endl;
+
+    }
+
+    return 0;*/
+
 }
